7f60a6573cfa4cfa79850cb9067d9bb184551cf7
jp2109 <113717377+jp2109@users.noreply.github.com>
2022-09-17 02:12:05 +0800

    Add files via upload

---
+++ TAR02.py
@@ -0,0 +1,9 @@
+n1 = int(input("Número 1: "))
+n2 = int(input("Número 2: "))
+print("\nSolución Paso a Paso\n")
+print("sqrt((" + str(n1) + "^2) + (" + str(n2) + "^2))")
+print("sqrt(" + str(n1 ** 2) + " + (" + str(n2) + "^2))")
+print("sqrt(" + str(n1 ** 2) + " + " + str(n2 ** 2) + ")")
+print("sqrt(" + str(n1 ** 2 + n2 ** 2) + ")")
+print("= " + str((n1 ** 2 + n2 ** 2) ** (1/2)))
+#Josue Perez#
